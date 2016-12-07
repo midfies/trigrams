b732f32809eadb59478f4583a85a31e3e7225612
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-07 14:23:05 -0800

    func passes remove punctuation test

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,10 +1,15 @@
 """a trigram algorithm that generates text using a book-sized file as input."""
 import io
+import string
+import re
+
 
 def main(file_path, num_words):
     data = input_file(file_path)
     sentences = split_data(data)
+    book_dic = build_dic(sentences)
     print(sentences)
+
 
 def input_file(path):
     file = io.open(path)
@@ -15,6 +20,16 @@ def input_file(path):
 def split_data(data):
     sentences = data.split('.')
     return sentences
+
+
+def remove_punc(sentences):
+    return re.sub('[%s]' % string.punctuation, '', sentences)
+
+
+def build_dic(sentences):
+    for sentence in sentences:
+        working_sentence = remove_punc(sentence)
+        print(working_sentence)
 
 
 if __name__ == '__main__':
