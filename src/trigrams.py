64205336d36a65234a341fc2c6a7a73df9a9b9b8
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-12-07 13:55:26 -0800

    func passes split data test

--- src/trigrams.py
+++ src/trigrams.py
@@ -3,13 +3,18 @@ import io
 
 def main(file_path, num_words):
     data = input_file(file_path)
-    print(data)
-
+    sentences = split_data(data)
+    print(sentences)
 
 def input_file(path):
     file = io.open(path)
     data = file.read()
     return data
+
+
+def split_data(data):
+    sentences = data.split('.')
+    return sentences
 
 
 if __name__ == '__main__':
