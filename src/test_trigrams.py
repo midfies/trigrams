0cf471b1cc9f155189b284528386e46f5f55b180
Marc Fieser <midfies@gmail.com>
2016-12-07 16:05:00 -0800

    build dic functionality working

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,12 +1,6 @@
 """Test module for trigrams.py."""
 
 # import pytest
-sample = """This * is - sentence one. This is sentence two. This is sentence \
-three."""
-sample_sent = 'This * is - sentence one'
-sample_stripped = "This  is  sentence one"
-empty_dic = {}
-dic = {'key': [1]}
 
 
 def test_input_file():
@@ -15,30 +9,37 @@ def test_input_file():
 
 
 def test_get_sentences():
+    sample = """This * is - sentence one. This is sentence two. This is sentence \
+three."""
     from trigrams import split_data
     assert split_data(sample)[0] == """This * is - sentence one"""
 
 
 def test_remove_punc():
+    sample_sent = 'This * is - sentence one'
     from trigrams import remove_punc
     assert '*' not in remove_punc(sample_sent)
 
 
 def test_get_words():
+    sample_split = "This  is  sentence one"
     from trigrams import split_words
-    assert split_words(sample_stripped) == ['This', 'is', 'sentence', 'one']
+    assert split_words(sample_split) == ['This', 'is', 'sentence', 'one']
 
 
 def test_add_to_empty_dic():
+    empty_dic = {}
     from trigrams import add_to_dic
     assert add_to_dic(empty_dic, 'key', 1) == {'key': [1]}
 
 
 def test_found_key():
+    dic = {'key': [1]}
     from trigrams import add_to_dic
     assert add_to_dic(dic, 'key', 2) == {'key': [1, 2]}
 
 
 def test_new_key():
+    dic = {'key': [1]}
     from trigrams import add_to_dic
-    assert add_to_dic(dic, 'key2', 3) == {'key': [1, 2], 'key2': [3]}
+    assert add_to_dic(dic, 'key2', 3) == {'key': [1], 'key2': [3]}
